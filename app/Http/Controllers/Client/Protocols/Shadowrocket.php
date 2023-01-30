8c3a0b739e8a8faeffde38a2edf1093efb40e663
darwinchow <69086749+darwinchow@users.noreply.github.com>
2023-01-31 00:48:48 +0800

    Squashed commit of the following:
    commit 86589b5c036c3d96263e99bc05c82ea11d12ab2d
    Author: darwinchow <69086749+darwinchow@users.noreply.github.com>
    Date:   Tue Jan 31 00:47:52 2023 +0800
    
        update: fix bugs
    
    commit a260e77ebda204b137e98dfb96d8fef7d70178f8
    Author: darwinchow <69086749+darwinchow@users.noreply.github.com>
    Date:   Tue Jan 31 00:46:32 2023 +0800
    
        update: fix bugs

--- app/Http/Controllers/Client/Protocols/Shadowrocket.php
+++ app/Http/Controllers/Client/Protocols/Shadowrocket.php
@@ -112,7 +112,7 @@ class Shadowrocket
             }
         }
         $query = http_build_query($config, '', '&', PHP_QUERY_RFC3986);
-        $uri = ($server['protocol'] === 'auto') ? "vless" : $server['protocol'] . "://{$userinfo}?{$query}";
+        $uri = (($server['protocol'] === 'auto') ? "vless" : $server['protocol']) . "://{$userinfo}?{$query}";
         $uri .= "\r\n";
         return $uri;
     }
